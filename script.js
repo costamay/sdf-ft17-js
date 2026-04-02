8c7cf1843dfeb78d1d1451efa337225b94495237
Titus <titusouko@gmail.com>
2026-04-02 10:43:36 +0300

    add event listener

--- script.js
+++ script.js
@@ -19,3 +19,35 @@
 
 document.getElementById('about').append(para)
  document.getElementById('paragraph').style.textAlign = "center"
+
+//  function buttonClick(){
+//    alert('I have been clicked')
+//  }
+
+// Event Listener
+// grab the target element
+let button = document.getElementById('button')
+
+// attached an event to the target
+// button.addEventListener(1 =>event Type, 2  => callback function)
+
+button.addEventListener('click', function(event){
+   console.log(event)
+
+   alert('I have been clicked!')
+
+})
+
+// console.log(button)
+
+let myName = document.getElementById('myName')
+
+let form = document.getElementById('form')
+
+form.addEventListener('submit', function(event){
+   event.preventDefault()
+  let name = document.getElementById('name').value
+  
+  myName.textContent = name
+
+})
